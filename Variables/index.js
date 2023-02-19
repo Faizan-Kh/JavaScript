c8164e9f64aceecef70dcb6df89115c4d967e7b3
Faizan <faizanswabi95@gmail.com>
2023-02-19 21:44:14 +0500

    initial commit

---
+++ Variables/index.js
@@ -0,0 +1,11 @@
+let firstname = "Faizan"; //Strings
+let age = 21; //numbers
+let student = true; //booleans
+age = age + 1;
+
+console.log("This is", firstname);
+console.log("I am",age,"years old");
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+document.getElementById("p2").innerHTML = "I am " + age + " years old.";
